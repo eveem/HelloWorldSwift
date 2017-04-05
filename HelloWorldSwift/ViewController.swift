43da59cb1563afee11db9d8f09928054daba8765
eveem <monpriya.tammavong@gmail.com>
2017-04-05 20:32:14 +0700

    Initial commit

---
+++ HelloWorldSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldSwift
+//
+//  Created by Monpriya on 4/5/2560 BE.
+//  Copyright © 2560 Monpriya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
